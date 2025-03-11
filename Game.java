5ef2ae065efadd2b1af8f23fe7d926a88cfc7092
DogeParker <83430363+DogeParker@users.noreply.github.com>
2025-03-11 09:56:28 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,8 +4,8 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Game extends JPanel implements KeyListener, Runnable {
-	private Level1 level1 = new Level1();
-	private final int SCREEN_WIDTH = 800;  // window width
+    private Level1 level1 = new Level1();
+    private final int SCREEN_WIDTH = 800;  // window width
     private final int SCREEN_HEIGHT = 600; // window height
     
     //hardcoded player values
